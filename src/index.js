1d2ccd8085eaf2dc661f5c6e8907db868101b32a
Angélica Venta <angelicaventap@gmail.com>
2021-07-08 20:15:28 -0500

    Card- validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import validator from './validator.js';
 
-console.log(validator);
+let inputValue
+let inputName
+let CardValidation
+
+document.querySelector("#buttonv").addEventListener("click",theFunction);
+function theFunction() {
+    inputName = document.getElementById("Username").value;
+    document.getElementById("nombre").innerHTML = inputName;
+    inputValue = document.getElementById("Cardnumber").value;
+    // console.log(inputValue);
+    let maskifycc = validator.maskify(inputValue);
+    document.getElementById("resultado").innerHTML = maskifycc;
+    if (inputValue == ""  || isNaN(inputValue)){
+      document.getElementById("resultado").innerHTML = "Los datos que ingresaste no son válidos, por favor ingresa un número";
+    }
+    else {
+      CardValidation = validator.isValid(inputValue);
+      if (CardValidation == true){
+        document.getElementById("validornot").innerHTML = "es un número de tarjeta VÁLIDO";
+      } else {
+        document.getElementById("validornot").innerHTML = "es un número de tarjeta NO VÁLIDO";
+      }
+
+    }
+}
+// console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,43 @@
 const validator = {
-  // ...
+  isValid(inputValue){
+    let numberOfDigits = inputValue.split("").map(Number).reverse();
+    for(let i = 0; i < numberOfDigits.length; i++) {
+      if (i%2 !== 0){
+        numberOfDigits[i] = numberOfDigits[i]*2;
+          if (numberOfDigits[i] > 9){
+          numberOfDigits[i] = numberOfDigits[i] - 9;
+          }
+       }
+    }
+    let suma = 0;
+    for(let i = 0; i < numberOfDigits.length; i++){
+      suma =  suma + numberOfDigits[i];
+      
+    }
+    let cardValidation = false;
+      if (suma%10 == 0){
+        cardValidation = true;
+       }
+    return cardValidation;
+  }, 
+
+
+  maskify(inputValue){
+    let digitsInput = inputValue.split("");
+      if (digitsInput.length < 4){
+        return inputValue;
+      } else {
+        let ccnomask = "";
+        for(let i = digitsInput.length-4; i < digitsInput.length; i++){
+          ccnomask += digitsInput[i];
+        }
+        let mask = "";
+        for(let i = 0; i < digitsInput.length-4; i++){
+          mask += "#";
+        }
+        return mask + ccnomask;
+      }
+  }
 };
 
 export default validator;
