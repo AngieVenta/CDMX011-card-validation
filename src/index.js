2f351022269acadaef37921d5bb2cfbe9ced7242
Angélica Venta <angelicaventap@gmail.com>
2021-07-12 00:47:27 -0500

    proyecto con hacker edition

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import validator from './validator.js';
 
-let inputValue
-let inputName
-let CardValidation
+var inputValue
 
 document.querySelector("#buttonv").addEventListener("click",theFunction);
 function theFunction() {
-    inputName = document.getElementById("Username").value;
+    const inputName = document.getElementById("username").value;
     document.getElementById("nombre").innerHTML = inputName;
-    inputValue = document.getElementById("Cardnumber").value;
+    inputValue = document.getElementById("cardnumber").value;
     // console.log(inputValue);
-    let maskifycc = validator.maskify(inputValue);
-    document.getElementById("resultado").innerHTML = maskifycc;
+    const maskifyCC = validator.maskify(inputValue);
+    document.getElementById("resultado").innerHTML = maskifyCC;
     if (inputValue == ""  || isNaN(inputValue)){
       document.getElementById("resultado").innerHTML = "Los datos que ingresaste no son válidos, por favor ingresa un número";
+      document.getElementById("validornot").innerHTML = "";
     }
     else {
-      CardValidation = validator.isValid(inputValue);
-      if (CardValidation == true){
-        document.getElementById("validornot").innerHTML = "es un número de tarjeta VÁLIDO";
+      const cardValidator = validator.isValid(inputValue);
+      const cardIssuer = validator.getIssuer(inputValue); 
+      if (cardValidator == true){
+        document.getElementById("validornot").innerHTML = "es un número de tarjeta VÁLIDO y su emisor es " + cardIssuer;
+        document.getElementById("theForm").reset(); 
       } else {
-        document.getElementById("validornot").innerHTML = "es un número de tarjeta NO VÁLIDO";
+        document.getElementById("validornot").innerHTML = "es un número de tarjeta NO VÁLIDO, verifica los datos ingresados";
       }
-
     }
 }
 // console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,6 @@
 const validator = {
   isValid(inputValue){
-    let numberOfDigits = inputValue.split("").map(Number).reverse();
+    const numberOfDigits = inputValue.split("").map(Number).reverse();
     for(let i = 0; i < numberOfDigits.length; i++) {
       if (i%2 !== 0){
         numberOfDigits[i] = numberOfDigits[i]*2;
@@ -23,7 +23,7 @@ const validator = {
 
 
   maskify(inputValue){
-    let digitsInput = inputValue.split("");
+    const digitsInput = inputValue.split("");
       if (digitsInput.length < 4){
         return inputValue;
       } else {
@@ -37,6 +37,27 @@ const validator = {
         }
         return mask + ccnomask;
       }
+  },
+
+  getIssuer(inputValue){
+    const cardLength = inputValue.length;   
+    let cardType = "";
+    if (inputValue.startsWith("4") && (cardLength == 15 || cardLength == 16)){
+      cardType = "Visa";
+      } else if (inputValue.startsWith("300") || inputValue.startsWith("301") || inputValue.startsWith("302") || inputValue.startsWith("303") || inputValue.startsWith("304") || inputValue.startsWith("305") || inputValue.startsWith("36") || inputValue.startsWith("38") && (cardLength > 13 || cardLength < 20)){
+        cardType = "Diner's Club";
+      } else if (inputValue.startsWith("34")|| inputValue.startsWith("37") && (cardLength == 15)){
+        cardType = "American Express";
+      } else if (inputValue.startsWith("1800") || inputValue.startsWith("2131") || inputValue.startsWith("3") && (cardLength > 15 || cardLength < 20)){
+        cardType = "JCB";
+      } else if (inputValue.startsWith("51") || inputValue.startsWith("52") || inputValue.startsWith("53") || inputValue.startsWith("54") || inputValue.startsWith("55") && (cardLength == 16)){
+        cardType = "MasterCard";
+      } else if (inputValue.startsWith("6011") && (cardLength > 15 || cardLength < 20)){
+        cardType = "Discover";
+      } else {
+        cardType = "no reconocido"
+      }
+    return cardType;
   }
 };
 
